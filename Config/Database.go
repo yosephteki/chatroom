a2946343ab8de7a1808f58b1d2761cf93f13f11e
Yosep Teki <yosephteki@gmail.com>
2021-06-19 10:09:34 +0700

    add DBconfig,DB struct,env variables

---
+++ Config/Database.go
@@ -0,0 +1,43 @@
+package Config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+type DBConfig struct {
+	host     string
+	port     string
+	user     string
+	dbname   string
+	password string
+	sslmode  string
+}
+
+func BuildConfig() *DBConfig {
+	dbConfig := DBConfig{
+		host:     os.Getenv("DB_host"),
+		port:     os.Getenv("DB_port"),
+		user:     os.Getenv("DB_user"),
+		password: os.Getenv("DB_password"),
+		dbname:   os.Getenv("DB_name"),
+		sslmode:  os.Getenv("DB_sslmode"),
+	}
+	return &dbConfig
+}
+
+func DBURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		dbConfig.host,
+		dbConfig.port,
+		dbConfig.user,
+		dbConfig.password,
+		dbConfig.dbname,
+		dbConfig.sslmode,
+	)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module chatroom
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+)
